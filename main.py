1c67bb0afa6e96873410d8ecce7e9f4f32624d1e
jooecoodes <jefeljoevillacorta@gmail.com>
2024-08-11 12:54:35 +0800

    commented examples

---
+++ main.py
@@ -0,0 +1,50 @@
+# Bubble Sort
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# Example usage
+# arr = [64, 34, 25, 12, 22, 11, 90]
+# print(bubble_sort(arr))
+
+#--------------------------------------------------------------------#
+# Merge Sort
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    return arr
+
+# Example usage
+arr = [64, 34, 25, 12, 22, 11, 90]
+print(merge_sort(arr))
